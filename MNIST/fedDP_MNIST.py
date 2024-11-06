d1bd6833aabb519e32375635df7b8622baa37de2
yuki-waseda <yuki.okura0405@gmail.com>
2024-11-06 21:34:14 +0900

    commit massage

--- MNIST/fedDP_MNIST.py
+++ MNIST/fedDP_MNIST.py
@@ -161,7 +161,6 @@ class server():
         self.model = t_model()
         #, sigmat = 1.12
         sigmat = np.sqrt(2 * np.log(1.25 / p_budget)) * 1 / epsilon +1.12
-        print(sigmat)
         self.sigmat = sigmat   
         self.n_clients = number_clients
         self.samples = get_samples(self.n_clients)
@@ -266,6 +265,7 @@ class server():
 #             clear_output()
             print('Comunication round: ', i)
             self.eval_acc()         
+            print(self.sigmat)
             rdp = compute_rdp(float(mt/len(self.clients)), self.sigmat, i, self.orders)
             _,delta_spent, opt_order = get_privacy_spent(self.orders, rdp, target_eps=self.epsilon)
             print('Delta spent: ', delta_spent)
