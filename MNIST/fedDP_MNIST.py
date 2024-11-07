ed97c715c419c013a3efb814161c29fb72047e26
yuki-waseda <yuki.okura0405@gmail.com>
2024-11-08 00:34:57 +0900

    commit massage

--- MNIST/fedDP_MNIST.py
+++ MNIST/fedDP_MNIST.py
@@ -163,7 +163,6 @@ class server():
         self.model = t_model()
         #sigmat = 0.55 * np.sqrt(2 * np.log(1.25 / p_budget)) * 1 / epsilon
         sigmat = 1.12 + np.sqrt(2 * np.log(1.25 / p_budget)) * 1 / epsilon
-        print(sigma)
         self.sigmat = sigmat   
         self.n_clients = number_clients
         self.samples = get_samples(self.n_clients)
