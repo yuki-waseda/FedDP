d9560647edf826701e6244adab03f06e071c240a
yuki-waseda <yuki.okura0405@gmail.com>
2024-12-11 08:32:59 +0900

    commit massage

--- MNIST/run_krum_MNIST.py
+++ MNIST/run_krum_MNIST.py
@@ -203,7 +203,9 @@ class server():
             total += equals.size(0)
             suma = suma + equals.sum().item()
         else:      
+            accuracy = suma/float(total)
             print('Accuracy: ',suma/float(total))
+        return accuracy
 
 
 
@@ -365,7 +367,7 @@ test_len = len(mnist_testset)
 valloader = torch.utils.data.DataLoader(mnist_testset, batch_size=64, shuffle=True)
 
 # 実行結果を保存するファイル名
-output_file = "nodp_result.csv"
+output_file = "krum_result.csv"
 
 # CSVファイルが存在しない場合にヘッダーを追加
 if not os.path.exists(output_file):
