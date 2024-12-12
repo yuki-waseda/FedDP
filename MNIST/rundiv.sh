5905fe9a3ed381f9ad91aba5c698055ae88c3a05
yuki-waseda <yuki.okura0405@gmail.com>
2024-12-12 13:03:36 +0900

    commit massage

--- MNIST/rundiv.sh
+++ MNIST/rundiv.sh
@@ -1,3 +1,3 @@
-/home/y.okura/local/python/bin/python3 run_divmalDP_MNIST.py ; 
-/home/y.okura/local/python/bin/python3 run_divkmeans_MNIST.py ; \
+/home/y.okura/local/python/bin/python3 run_divmalDP_MNIST.py; \
+/home/y.okura/local/python/bin/python3 run_divkmeans_MNIST.py; \
 /home/y.okura/local/python/bin/python3 run_divkrum_MNIST.py
