9117add41aa8d7b9e816503be9c16bd70d24684f
yuki-waseda <yuki.okura0405@gmail.com>
2024-12-13 11:05:02 +0900

    commit massage

--- MNIST/krum_MNIST.py
+++ MNIST/krum_MNIST.py
@@ -366,7 +366,7 @@ valloader = torch.utils.data.DataLoader(mnist_testset, batch_size=64, shuffle=Tr
 p_budget = 0.001
 epsilon = 8
 gamma = 0.01
-# 0.1で100%検出
+# 0.1, 0.03で100%検出
 serv = server(num_clients, p_budget,epsilon ,gamma)
 model = serv.server_exec(30)
 
