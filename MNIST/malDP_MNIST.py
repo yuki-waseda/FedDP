0cedc7704f3a4fdc8671c51f5b14d24be7890b79
yuki-waseda <yuki.okura0405@gmail.com>
2024-12-14 08:02:17 +0900

    commit massage

--- MNIST/malDP_MNIST.py
+++ MNIST/malDP_MNIST.py
@@ -331,7 +331,7 @@ valloader = torch.utils.data.DataLoader(mnist_testset, batch_size=64, shuffle=Tr
 #We're creating the Server class. A priv_budget of 0.001 (the max delta) and a Epsilon of 8
 # デルタバジェットBとプライバシー予算εを指定
 p_budget = 0.001
-epsilon = 8
+epsilon = 1
 gamma = 0
 serv = server(num_clients, p_budget, epsilon, gamma)
 model = serv.server_exec(30)
