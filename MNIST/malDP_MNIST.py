0aaf217db6cab64ef0bf5f86ea56e8bbede58dac
yuki-waseda <yuki.okura0405@gmail.com>
2024-12-14 08:08:29 +0900

    commit massage

--- MNIST/malDP_MNIST.py
+++ MNIST/malDP_MNIST.py
@@ -240,9 +240,11 @@ class server():
                 #else: 
                 #    noise = (np.random.normal(0, float((sigma)*(S_value)/np.sqrt(30)), size = deltas[i][key].shape))
                 if any(i < m for m in malModel) :
-                    noise = (np.random.normal((np.sqrt(2*gamma/30)*(sigma**2))*(S_value**2))), float((sigma**2))*(S_value**2))/np.sqrt(30)), size = deltas[i][key].shape)) 
+                    noise = (np.random.normal((np.sqrt(2*gamma)*(sigma*S_value)/30), float((sigma**2)*(S_value**2)/np.sqrt(30)), size = deltas[i][key].shape))
+                
                 else: 
-                    noise = (np.random.normal(0, float((sigma**2))*(S_value**2))/np.sqrt(30)), size = deltas[i][key].shape))
+                    noise = (np.random.normal(0, float((sigma**2)*(S_value**2)/np.sqrt(30)), size = deltas[i][key].shape))
+            
                 clippedDelta = clippedDelta.cpu().numpy()
                 modelSum = clippedDelta + noise
                 sanitized_deltas[i][key] = torch.from_numpy(modelSum).float().to('cpu')
