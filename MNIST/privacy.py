750f173d448676d5412e108865a825bcc8e43cd2
yuki-waseda <yuki.okura0405@gmail.com>
2024-12-16 13:56:07 +0900

    commit massage

--- MNIST/privacy.py
+++ MNIST/privacy.py
@@ -33,7 +33,7 @@ for epsilon in [1,2,4,8]:
             break
         orders = ( [1.25, 1.5, 1.75, 2., 2.25, 2.5, 3., 3.5, 4., 4.5] +
                     list(range(5, 64)) + [128, 256, 512])
-        rdp = compute_rdp(mt/clients, sigmat/2, i, orders)
+        rdp = compute_rdp(mt/clients, sigmat*0.6, i, orders)
         _,delta_spent, opt_order = get_privacy_spent(orders, rdp, target_eps=epsilon)
         
         print('Round: ',i)
