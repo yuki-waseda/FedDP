e6cec2d78d28af7a2d01814f66e1ea96dd22a5c9
yuki-waseda <yuki.okura0405@gmail.com>
2024-12-16 13:38:07 +0900

    commit massage

--- MNIST/privacy.py
+++ MNIST/privacy.py
@@ -24,8 +24,8 @@ clients = 100
 
 for epsilon in [0.5,1,2,4,8]:
     
-    sigmat = np.sqrt(2 * np.log(1.25 / p_budget)) * 1 /epsilon 
-    #sigmat = 1.12
+    #sigmat = np.sqrt(2 * np.log(1.25 / p_budget)) * 1 /epsilon +1.12 
+    sigmat = 1.12
     print('eps: ',epsilon)
     print('eps: ',sigmat)
     i=1
