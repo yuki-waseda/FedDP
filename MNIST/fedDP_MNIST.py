d64528883cbe91e3dd547aa2a910a4173c1efaf3
yuki-waseda <yuki.okura0405@gmail.com>
2024-12-16 13:53:49 +0900

    commit massage

--- MNIST/fedDP_MNIST.py
+++ MNIST/fedDP_MNIST.py
@@ -159,10 +159,10 @@ def noiseGen(mu, sigma, suma):
 #サーバークラス
 class server():
     def __init__(self, number_clients, p_budget, epsilon):
-        #sigmat = 1.12
+        sigmat = 1.12
         self.model = t_model()
         #sigmat = 0.55 * np.sqrt(2 * np.log(1.25 / p_budget)) * 1 / epsilon
-        sigmat = 1.12 + np.sqrt(2 * np.log(1.25 / p_budget)) * 1 / epsilon
+        #sigmat = 1.12 + np.sqrt(2 * np.log(1.25 / p_budget)) * 1 / epsilon
         self.sigmat = sigmat   
         self.n_clients = number_clients
         self.samples  = get_samples(self.n_clients)
